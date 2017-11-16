0f4518fa796a6a5101fca44d54b7bb96c9613527
nicholaswasicskocc21 <33705937+nicholaswasicskocc21@users.noreply.github.com>
2017-11-15 20:51:58 -0500

    Create multiple_channels.py

---
+++ multiple_channels.py
@@ -0,0 +1,14 @@
+import time
+import RPi.GPIO as IO
+
+IO.setmode(IO.BOARD)
+IO.setwarnings(false)
+IO.setup(IO.OUT)
+
+while True:
+  Chan_List[40, 38]
+  IO.output(Chan_List, IO.HIGH)
+  time.sleep(5)
+  
+  IO.output(Chan_List, IO.LOW)
+  time.sleep(5)
